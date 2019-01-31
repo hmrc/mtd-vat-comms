2db41edbd0aafb7d9a4491b2c7acef7eab1b3756
Duane Miles <29195796+TheDeadlyPianist@users.noreply.github.com>
2019-01-31 13:46:50 +0000

    [DM] master Updated dependencies and added scalastyle config

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,17 +6,16 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-
     "uk.gov.hmrc"             %% "play-reactivemongo"       % "6.2.0",
     "uk.gov.hmrc"             %% "bootstrap-play-25"        % "4.8.0"
   )
 
   val test = Seq(
-    "org.scalatest"           %% "scalatest"                % "3.0.4"                 % "test",
+    "org.scalatest"           %% "scalatest"                % "3.2.0-SNAP10"          % "test",
     "com.typesafe.play"       %% "play-test"                % current                 % "test",
     "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
     "uk.gov.hmrc"             %% "service-integration-test" % "0.2.0"                 % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "2.0.0"                 % "test, it"
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.0"                 % "test, it"
   )
 
 }
