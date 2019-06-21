a6682257146d1c92a01bc4c576bc9c93cc134907
davidmthornton <28890110+davidmthornton@users.noreply.github.com>
2019-06-21 20:41:30 +0100

    Added route and controller for opt out

---
+++ test/controllers/OptOutControllerSpec.scala
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.BaseSpec
+import common.ApiConstants._
+import common.VatChangeEventConstants._
+import mocks.MockCommsEventService
+import models.VatChangeEvent
+import play.api.http.Status.{BAD_REQUEST, INTERNAL_SERVER_ERROR, NO_CONTENT}
+import play.api.libs.json.JsObject
+import play.api.mvc.Result
+
+import scala.concurrent.Future
+
+class OptOutControllerSpec extends BaseSpec with MockCommsEventService {
+
+  val controller = new OptOutController(mockCommsEventService)
+
+  val testRequestJson: JsObject        = vatChangeEventJson("Opt Out Change")
+  val testRequestModel: VatChangeEvent = vatChangeEventModel("Opt Out Change")
+
+  "The handleEvent action" when {
+
+    "valid JSON is received" should {
+
+      "the vat opt out event was successfully added to the queue" should {
+
+        "return 204" in {
+          mockQueueRequest(testRequestModel)(Future.successful(true))
+          val result: Result = controller.handleEvent(request.withJsonBody(testRequestJson))
+
+          status(result) shouldBe NO_CONTENT
+        }
+      }
+
+      "the vat opt out event was unsuccessfully added to the queue" should {
+
+        "return 500" in {
+          mockQueueRequest(testRequestModel)(Future.successful(false))
+          val result: Result = controller.handleEvent(request.withJsonBody(testRequestJson))
+
+          status(result) shouldBe INTERNAL_SERVER_ERROR
+        }
+      }
+    }
+
+    "invalid JSON is received" should {
+
+      "return 400" in {
+        val result: Result = controller.handleEvent(request.withJsonBody(invalidJsonRequest))
+        status(result) shouldBe BAD_REQUEST
+      }
+
+      "return JSON describing the error" in {
+        val result: Result = controller.handleEvent(request.withJsonBody(invalidJsonRequest))
+        jsonBodyOf(result) shouldBe invalidJsonResponse
+      }
+    }
+
+    "something other than JSON is received" should {
+
+      "return 400" in {
+        val result: Result = controller.handleEvent(request.withBody(invalidRequestBody))
+        status(result) shouldBe BAD_REQUEST
+      }
+
+      "return JSON describing the error" in {
+        val result: Result = controller.handleEvent(request.withBody(invalidRequestBody))
+        jsonBodyOf(result) shouldBe invalidFormatResponse
+      }
+    }
+  }
+}
