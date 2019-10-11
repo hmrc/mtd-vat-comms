4645abc1b9d76031393cb8c028bb8be5e96b01b4
Lewis Hay <29063729+lewishay@users.noreply.github.com>
2019-10-11 12:31:37 +0100

    Added secure messages for website approved/rejected

---
+++ test/views/VatWebsiteApprovedViewSpec.scala
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
+class VatWebsiteApprovedViewSpec extends ViewBaseSpec {
+
+  val website = "http://www.website.com"
+
+  "The website approved message" when {
+
+    "a principal entity has requested to change their website" should {
+
+      lazy val view = views.html.vatWebsiteApproved(isTransactor = false, Some(website))
+      lazy implicit val document: Document = Jsoup.parse(view.body)
+
+      "have the correct paragraph" in {
+        elementText("p") shouldBe s"Your new website address for VAT is: $website"
+      }
+    }
+
+    "a principal entity has requested to remove their website" should {
+
+      lazy val view = views.html.vatWebsiteApproved(isTransactor = false, None)
+      lazy implicit val document: Document = Jsoup.parse(view.body)
+
+      "have the correct paragraph" in {
+        elementText("p") shouldBe "You can add a website address for VAT on your business details page."
+      }
+    }
+
+    "an agent has requested to remove their client's website" should {
+
+      lazy val view = views.html.vatWebsiteApproved(isTransactor = true, None)
+      lazy implicit val document: Document = Jsoup.parse(view.body)
+
+      "have the correct paragraph" in {
+        elementText("p") shouldBe "You or your agent can add a website address for VAT on your business details page."
+      }
+
+      "have a link to the ChoC overview page" which {
+
+        "has the correct text" in {
+          elementText("a") shouldBe "add a website address for VAT"
+        }
+
+        "has the correct link location" in {
+          element("a").attr("href") shouldBe mockAppConfig.manageVatSubscriptionUrl
+        }
+      }
+    }
+  }
+}
---
+++ test/views/VatWebsiteRejectedViewSpec.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
+class VatWebsiteRejectedViewSpec extends ViewBaseSpec {
+
+  "The website rejected message" when {
+
+    "the user has attempted to change their website" should {
+
+      lazy val view = views.html.vatWebsiteRejected(isRemoval = false)
+      lazy implicit val document: Document = Jsoup.parse(view.body)
+
+      "have the correct first paragraph" in {
+        elementText("p:nth-of-type(1)") shouldBe "We have rejected the change to your website address for VAT."
+      }
+
+      "have the correct h2" in {
+        elementText("h2") shouldBe "What happens next"
+      }
+
+      "have the correct second paragraph" in {
+        elementText("p:nth-of-type(2)") shouldBe
+          "If you do not agree with our decision, you can ask for a review by an HMRC officer not previously involved " +
+            "in the matter. If you want a review, you should write to us within 30 days of receiving this message " +
+            "giving the reasons why you do not agree with our decision. Write to:"
+      }
+
+      "have the correct HMRC address as the final paragraph" in {
+        elementText("p:nth-of-type(3)") shouldBe
+          "HMRC VAT Registration Service Crown House Birch Street WOLVERHAMPTON WV1 4JX"
+      }
+    }
+
+    "the user has attempted to remove their website" should {
+
+      lazy val view = views.html.vatWebsiteRejected(isRemoval = true)
+      lazy implicit val document: Document = Jsoup.parse(view.body)
+
+      "have the correct first paragraph" in {
+        elementText("p:nth-of-type(1)") shouldBe "We have rejected the removal of your website address for VAT."
+      }
+    }
+  }
+}
